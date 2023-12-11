3ffb19ef7fc2f09827f23873d02096de3fd1d848
Peter A. Labib <peteramir5522@outlook.com>
2023-12-11 19:24:51 +0200

    feat: watch anime basic layout

--- src/app/library/anime-watch/anime-watch.component.ts
+++ src/app/library/anime-watch/anime-watch.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-anime-watch',
   templateUrl: './anime-watch.component.html',
-  styleUrls: ['./anime-watch.component.scss']
+  styleUrls: ['./anime-watch.component.scss'],
 })
-export class AnimeWatchComponent implements OnInit {
+export class AnimeWatchComponent implements OnDestroy {
+  @ViewChild('animeTitle') animeTitle: ElementRef;
+  activeIndex = +this.activeRoute.snapshot.queryParams.episodeIndex;
+  anime = this.activeRoute.snapshot.data.anime;
+  episodes: string[] = [...this.anime.episodes];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, private activeRoute: ActivatedRoute) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
 
+  private getEpisodesMimeType(currentEpisode: string): string | undefined {
+    const episodeExtension = currentEpisode.toLowerCase().split('.')[
+      currentEpisode.split('.').length - 1
+    ];
+    if (episodeExtension === 'flv') return 'video/x-flv';
+    else if (episodeExtension === 'mkv') return 'video/x-matroska';
+    else if (episodeExtension === 'mwv') return 'video/x-matroska';
+    else if (episodeExtension === 'mp4') return 'video/mp4';
+    else return undefined;
+  }
+
+  ngOnDestroy(): void {
+    // Restore route reuse strategy
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+  }
 }
--- src/app/library/anime-watch/anime-watch.module.ts
+++ src/app/library/anime-watch/anime-watch.module.ts
@@ -3,9 +3,27 @@ import { CommonModule } from '@angular/common';
 
 import { AnimeWatchComponent } from './anime-watch.component';
 import { AnimeWatchRoutingModule } from './anime-watch.routing';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { NgIconsModule } from '@ng-icons/core';
+import {
+  heroArrowSmallLeft,
+  heroArrowSmallRight,
+} from '@ng-icons/heroicons/outline';
+import { heroArrowLeft } from '@ng-icons/heroicons/outline';
+import { A11yModule } from '@angular/cdk/a11y';
 
 @NgModule({
   declarations: [AnimeWatchComponent],
-  imports: [CommonModule, AnimeWatchRoutingModule],
+  imports: [
+    CommonModule,
+    ScrollingModule,
+    A11yModule,
+    AnimeWatchRoutingModule,
+    NgIconsModule.withIcons({
+      heroArrowSmallRight,
+      heroArrowSmallLeft,
+      heroArrowLeft
+    }),
+  ],
 })
 export class AnimeWatchModule {}
