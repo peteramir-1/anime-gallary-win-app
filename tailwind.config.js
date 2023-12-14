dee760607d085a1e726069b68eb018f68575c8ef
Peter A. Labib <peteramir5522@outlook.com>
2023-12-14 17:07:03 +0200

    fix: Tailwindcss config aspect ratio conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -74,7 +74,7 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
     // require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
+    // require('@tailwindcss/aspect-ratio'),
     require('tailwindcss-animated'),
   ],
 };
