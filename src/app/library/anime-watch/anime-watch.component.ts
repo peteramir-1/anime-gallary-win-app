d73486ff079df77a6ec529df82dce552490ad638
Peter A. Labib <peteramir5522@outlook.com>
2023-12-14 20:05:05 +0200

    feat: add videojs player

--- src/app/library/anime-watch/anime-watch.component.ts
+++ src/app/library/anime-watch/anime-watch.component.ts
@@ -1,21 +1,96 @@
-import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+
+import 'videojs-playlist/dist/videojs-playlist.min.js';
+import 'videojs-playlist-ui/dist/videojs-playlist-ui.min.js';
+import 'videojs-hotkeys/videojs.hotkeys.min.js';
+import videojs from 'video.js';
 
 @Component({
   selector: 'app-anime-watch',
   templateUrl: './anime-watch.component.html',
   styleUrls: ['./anime-watch.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AnimeWatchComponent implements OnDestroy {
-  @ViewChild('animeTitle') animeTitle: ElementRef;
-  activeIndex = +this.activeRoute.snapshot.queryParams.episodeIndex;
+export class AnimeWatchComponent implements AfterViewInit {
+  private episodeIndex: number =
+    +this.activeRoute.snapshot.queryParams.episodeIndex;
+  get currentEpisode() {
+    return this.episodeIndex + 1;
+  }
   anime = this.activeRoute.snapshot.data.anime;
-  episodes: string[] = [...this.anime.episodes];
+  player: any;
 
-  constructor(private router: Router, private activeRoute: ActivatedRoute) {
-    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+  constructor(private router: Router, private activeRoute: ActivatedRoute) {}
+
+  ngAfterViewInit(): void {
+    this.videoJsInit();
+    this.setVideoJsEvents();
+    this.videoJsPlaylistInit();
+    this.videoJsPlaylistUiInit();
   }
 
+  private videoJsInit() {
+    const vjsConfigurations = {
+      controls: true,
+      autoplay: false,
+      controlBar: { remainingTimeDisplay: { displayNegative: false } },
+      preload: 'auto',
+      poster: this.anime.thumbnail,
+      controllBar: { pictureInPictureToggle: false },
+      plugins: {
+        hotkeys: {
+          volumeStep: 0.1,
+          seekStep: 5,
+          enableModifiersForNumbers: false,
+        },
+      },
+    };
+    this.removeSynamicVjsStyles();
+    this.setupVjsPlayer(vjsConfigurations);
+  }
+  private removeSynamicVjsStyles() {
+    (window as any).VIDEOJS_NO_DYNAMIC_STYLE = true;
+  }
+  private setupVjsPlayer(configs: any) {
+    this.player = videojs('main-video-js', configs);
+  }
+
+  private setVideoJsEvents() {
+    // Add playlist menu ui events
+    this.player.on('playlistitem', () => {
+      this.episodeIndex = this.player.playlist.currentIndex();
+      this.scrollPlaylistMenuToCurrentEpisode();
+      this.router.navigate([], {
+        relativeTo: this.activeRoute,
+        queryParams: { episodeIndex: this.player.playlist.currentIndex() },
+      });
+    });
+  }
+  private scrollPlaylistMenuToCurrentEpisode() {
+    document
+      .querySelector('#vjs-playlist-ui-container')
+      .scrollTo(0, 117 * this.episodeIndex);
+  }
+
+  private videoJsPlaylistInit() {
+    // Setup playlist
+    this.player.playlist(this.getAnimePlaylist(), this.episodeIndex);
+    this.player.playlist.autoadvance(0);
+  }
+  private getAnimePlaylist() {
+    return this.anime.episodes.map((episode, i) => ({
+      sources: [
+        {
+          src:
+            'http://localhost:8020/' + episode.split('\\').slice(1).join('/'),
+          type: this.getEpisodesMimeType(episode),
+        },
+      ],
+      thumbnail: this.anime.thumbnail,
+      name: `Episode ${i + 1}`,
+    }));
+  }
   private getEpisodesMimeType(currentEpisode: string): string | undefined {
     const episodeExtension = currentEpisode.toLowerCase().split('.')[
       currentEpisode.split('.').length - 1
@@ -27,8 +102,9 @@ export class AnimeWatchComponent implements OnDestroy {
     else return undefined;
   }
 
-  ngOnDestroy(): void {
-    // Restore route reuse strategy
-    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+  private videoJsPlaylistUiInit() {
+    this.player.playlistUi({
+      el: document.getElementById('vjs-playlist'),
+    });
   }
 }
