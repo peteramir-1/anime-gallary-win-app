3f325232885003433a65b037a7ba983a2f803a23
Peter A. Labib <peteramir5522@outlook.com>
2023-12-16 17:15:43 +0200

    ref: Separate videojs player settings in separate service

---
+++ src/app/library/anime-watch/video-player.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import 'videojs-playlist/dist/videojs-playlist.min.js';
+import 'videojs-playlist-ui/dist/videojs-playlist-ui.min.js';
+import 'videojs-hotkeys/videojs.hotkeys.min.js';
+import videojs from 'video.js';
+
+interface Episode {
+  src: string;
+  type: string;
+}
+
+interface Playlist {
+  sources: Episode[];
+  thumbnail: string;
+  name: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VideoPlayerService {
+  player: any;
+  configurations = {
+    controls: true,
+    autoplay: false,
+    controlBar: { remainingTimeDisplay: { displayNegative: false } },
+    preload: 'auto',
+    controllBar: { pictureInPictureToggle: false },
+    plugins: {
+      hotkeys: {
+        volumeStep: 0.1,
+        seekStep: 5,
+        enableModifiersForNumbers: false,
+      },
+    },
+  };
+  constructor() {}
+
+  videoJsInit(element: string | HTMLVideoElement, configs: { poster: string }) {
+    this.removeSynamicVjsStyles();
+    this.setupVjsPlayer(element, this.getConfigurations(configs.poster));
+  }
+  private removeSynamicVjsStyles() {
+    (window as any).VIDEOJS_NO_DYNAMIC_STYLE = true;
+  }
+  private setupVjsPlayer(element: string | HTMLVideoElement, configs: any) {
+    this.player = videojs(element, configs);
+  }
+  private getConfigurations(poster: string) {
+    return { ...this.configurations, poster };
+  }
+
+  videoJsPlaylistInit(playlist: Playlist, episodeIndex: number) {
+    // Setup playlist
+    this.player.playlist(playlist, episodeIndex);
+    this.player.playlist.autoadvance(0);
+  }
+
+  videoJsPlaylistUiInit() {
+    this.player.playlistUi({
+      el: document.getElementById('vjs-playlist'),
+    });
+  }
+
+  on(event: string, callback: () => void) {
+    this.player.on(event, callback);
+  }
+
+  next() {
+    this.player.playlist.next();
+  };
+
+  previous() {
+    this.player.playlist.previous();
+  }
+
+  dispose() {
+    this.player.dispose();
+  }
+}
