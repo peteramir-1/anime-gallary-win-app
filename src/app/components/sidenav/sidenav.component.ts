15ca5fef819536c880beb6f632cc2fd47809d9b2
Peter A. Labib <peteramir5522@outlook.com>
2023-12-16 16:07:13 +0200

    ref: separate dark mode logic in separate service

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -6,6 +6,7 @@ import {
   Component,
 } from '@angular/core';
 import { sidenav } from './models/sidenav';
+import { DarkModeService } from 'src/app/services/dark-mode.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -14,10 +15,13 @@ import { sidenav } from './models/sidenav';
 })
 export class SidenavComponent implements OnInit {
   sidenav = sidenav;
-  darkMode = false;
+  darkMode = this.darkModeService.darkMode;
   @ViewChild('lightModeBtn') lightModeBtn: ElementRef;
   @ViewChild('darkModeBtn') darkModeBtn: ElementRef;
-  constructor(private renderer: Renderer2) {}
+  constructor(
+    public darkModeService: DarkModeService,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
     setTimeout(() => {
@@ -25,53 +29,15 @@ export class SidenavComponent implements OnInit {
     });
   }
 
-  private setTheme() {
-    if (localStorage.getItem('theme') === 'light') {
-      this.applyLightModeClasses();
-      this.darkMode = false;
-      return;
-    }
-    if (localStorage.getItem('theme') === 'dark') {
-      this.applyDarkModeClasses();
-      this.darkMode = true;
-      return;
-    }
-    if (window.matchMedia('(prefers-color-scheme: light)').matches) {
-      this.applyLightModeClasses();
-      this.darkMode = false;
-    } else {
-      this.applyDarkModeClasses();
-      this.darkMode = true;
-    }
-  }
-
-  private applyDarkModeClasses() {
-    this.renderer.addClass(document.querySelector('html'), 'dark');
-    this.renderer.removeClass(this.darkModeBtn.nativeElement, 'hidden');
-    this.renderer.addClass(this.lightModeBtn.nativeElement, 'hidden');
-  }
-
-  private applyLightModeClasses() {
-    this.renderer.removeClass(document.querySelector('html'), 'dark');
-    this.renderer.addClass(this.darkModeBtn.nativeElement, 'hidden');
-    this.renderer.removeClass(this.lightModeBtn.nativeElement, 'hidden');
-  }
-
-  async switchToLightTheme() {
-    await localStorage.setItem('theme', 'light');
-    this.setTheme();
-  }
-
-  async switchToDarkTheme() {
-    await localStorage.setItem('theme', 'dark');
-    this.setTheme();
-  }
-
-  toggleTheme() {
-    if (this.darkMode) {
-      this.switchToLightTheme();
-    } else {
-      this.switchToDarkTheme();
-    }
+  setTheme() {
+    this.darkMode.subscribe(darkMode => {
+      if (darkMode) {
+        this.renderer.removeClass(this.darkModeBtn.nativeElement, 'hidden');
+        this.renderer.addClass(this.lightModeBtn.nativeElement, 'hidden');
+      } else {
+        this.renderer.addClass(this.darkModeBtn.nativeElement, 'hidden');
+        this.renderer.removeClass(this.lightModeBtn.nativeElement, 'hidden');
+      }
+    });
   }
 }
