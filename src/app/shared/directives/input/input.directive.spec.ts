e351423fedeeb83f39610164fd0b1e2ceb8f7c84
Peter A. Labib <peteramir5522@outlook.com>
2024-03-19 23:49:12 +0200

    feat: create appInput to replace input classes

---
+++ src/app/shared/directives/input/input.directive.spec.ts
@@ -0,0 +1,43 @@
+import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectionStrategy, Component, DebugElement, ViewChild } from '@angular/core';
+import { InputDirective } from './input.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { InputModule } from './inputs.module';
+
+@Component({
+  selector: 'test-element',
+  template: `<input type="text" [appInput] />`,
+})
+class testComponent {
+  @ViewChild(InputDirective) directive: InputDirective;
+  constructor() {}
+}
+
+describe('InputDirective', () => {
+  let fixture: ComponentFixture<testComponent>;
+  let component: testComponent;
+  let element: DebugElement;
+
+  
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [testComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [CommonModule, InputModule],
+    })
+      .overrideComponent(testComponent, {
+        set: { changeDetection: ChangeDetectionStrategy.Default },
+      })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(testComponent);
+    component = fixture.componentInstance;
+    fixture.autoDetectChanges();
+    fixture.detectChanges();
+    element = fixture.debugElement.children[0];
+  });
+
+  it('should create component successfully', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/directives/input/input.directive.ts
@@ -0,0 +1,38 @@
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  Input,
+  OnInit,
+  inject,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appInput]',
+  host: {
+    class:
+      'w-full rounded-lg p-2 text-xs ring-1 input-bg-default input-ring-default input-text-default focus:ring-2 focus:input-primary-ring-default dark:input-bg-dark dark:input-ring-dark dark:input-text-dark dark:focus:input-primary-ring-dark invalid:ring-2 invalid:input-error-bg-default invalid:input-error-ring-default invalid:input-error-text-default invalid:placeholder:input-error-text-default dark:invalid:input-error-bg-dark dark:invalid:input-error-ring-dark dark:invalid:input-error-text-dark dark:invalid:placeholder:input-error-text-dark',
+  },
+})
+export class InputDirective implements OnInit {
+  private readonly elementTagName = inject(ElementRef).nativeElement.tagName;
+
+  @HostBinding('class.resize-none') resizeNone =
+    this.elementTagName === 'TEXTAREA';
+  @HostBinding('class.h-full') heightFull = this.elementTagName === 'TEXTAREA';
+  @Input() type: string;
+
+  ngOnInit(): void {
+    this.checkForWrongElementError();
+  }
+
+  private checkForWrongElementError(): void {
+    if (!['INPUT', 'TEXTAREA'].includes(this.elementTagName)) {
+      const wrongElement = new Error(
+        'Directive has been applied on a wrong element. This directive is applied only on input elements.'
+      );
+      wrongElement.name = 'ErrorWrong Element';
+      throw wrongElement;
+    }
+  }
+}
