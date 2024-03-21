f19a70eaf1ab811f5e75bb4ea41ada97bedce790
Peter A. Labib <peteramir5522@outlook.com>
2024-03-21 20:07:26 +0200

    feat: support column layout setup for app-form-field component

--- src/app/shared/modules/inputs/form-field/form-field.component.ts
+++ src/app/shared/modules/inputs/form-field/form-field.component.ts
@@ -1,4 +1,9 @@
-import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
+import {
+  BooleanInput,
+  NumberInput,
+  coerceBooleanProperty,
+  coerceNumberProperty,
+} from '@angular/cdk/coercion';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
@@ -17,6 +22,30 @@ export class FormFieldComponent {
     this._required = coerceBooleanProperty(value);
   }
   private _required = false;
+  @Input({ required: false })
+  get flexCol() {
+    return this._flexCol;
+  }
+  set flexCol(value: BooleanInput) {
+    this._flexCol = coerceBooleanProperty(value);
+  }
+  private _flexCol = true;
+  @Input({ required: false })
+  get flexGap() {
+    return this._flexGap;
+  }
+  set flexGap(value: NumberInput) {
+    this._flexGap = coerceNumberProperty(value);
+  }
+  private _flexGap = 0;
+  @Input({ required: false })
+  get labelFixedWidthPercentage() {
+    return this._labelFixedWidthPercentage;
+  }
+  set labelFixedWidthPercentage(value: NumberInput) {
+    this._labelFixedWidthPercentage = coerceNumberProperty(value);
+  }
+  private _labelFixedWidthPercentage = 0;
   @Input({ required: false }) for;
   @Input({ required: false }) help?: string;
 }
