d9ee7e767bcc38e48e3382213a891ec42c8d112b
Peter A. Labib <peteramir5522@outlook.com>
2024-06-29 05:28:22 +0300

    feat: connect animer-viewer feature with backened

--- src/app/features/anime-viewer/anime-viewer.component.ts
+++ src/app/features/anime-viewer/anime-viewer.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ElectronService } from 'src/app/core/services/electron.service';
+import { GetAnimesFromFolderGQL } from 'src/app/core/services/graphql.service';
 
 @Component({
   selector: 'app-anime-viewer',
@@ -7,23 +9,30 @@ import { ElectronService } from 'src/app/core/services/electron.service';
   styleUrl: './anime-viewer.component.scss',
 })
 export class AnimeViewerComponent {
-  animesFolder = '';
-  filteredAnimes = [];
-  
-  private prevAnimeFolder = ''
+  animesFolder = new FormControl<string | undefined>(undefined);
+  animes = [];
+
+  private prevAnimeFolder = undefined;
   private electronService = inject(ElectronService);
+  private getAnimesFromFolderGQL = inject(GetAnimesFromFolderGQL);
 
   selectAnimeFolder(): void {
     this.electronService.selectFolder().then((path: string | false) => {
       if (!path || path === '') return;
-      this.animesFolder = path;
+      this.animesFolder.setValue(path);
     });
   }
 
   updateAnimesList(): void {
-    if(this.animesFolder !== this.prevAnimeFolder) {
-      // Update animes list 
+    if (this.animesFolder.value !== this.prevAnimeFolder) {
+      this.getAnimesFromFolderGQL
+        .fetch({ folderPath: this.animesFolder.value })
+        .subscribe(animes => {
+          if (animes.error) return;
+          this.animes = animes.data.animesFromFolder;
+        });
     }
-    this.prevAnimeFolder = this.animesFolder;
-  } 
+    console.log("there's duplicate inputs");
+    this.prevAnimeFolder = this.animesFolder.value;
+  }
 }
