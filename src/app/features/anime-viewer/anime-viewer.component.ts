578057f005a9eadfc28bcfedaf76dbc8082a75da
Peter A. Labib <peteramir5522@outlook.com>
2024-06-30 06:47:58 +0300

    feat: fetch pictures from file-serving backend feature

--- src/app/features/anime-viewer/anime-viewer.component.ts
+++ src/app/features/anime-viewer/anime-viewer.component.ts
@@ -1,6 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { FormControl } from '@angular/forms';
+
+import { map } from 'rxjs/operators';
+
 import { ElectronService } from 'src/app/core/services/electron.service';
+import { FileServingService } from 'src/app/core/services/file-serving.service';
 import { GetAnimesFromFolderGQL } from 'src/app/core/services/graphql.service';
 
 @Component({
@@ -15,6 +19,7 @@ export class AnimeViewerComponent {
   private prevAnimeFolder = undefined;
   private electronService = inject(ElectronService);
   private getAnimesFromFolderGQL = inject(GetAnimesFromFolderGQL);
+  private fileServingService = inject(FileServingService);
 
   selectAnimeFolder(): void {
     this.electronService.selectFolder().then((path: string | false) => {
@@ -24,12 +29,25 @@ export class AnimeViewerComponent {
   }
 
   updateAnimesList(): void {
+    const defaultThumbnail = '../../../assets/pictures/no-image.webp';
+
     if (this.animesFolder.value !== this.prevAnimeFolder) {
       this.getAnimesFromFolderGQL
         .fetch({ folderPath: this.animesFolder.value })
+        .pipe(
+          map(res => res?.data?.animesFromFolder || []),
+          map(animes => {
+            return animes.map(anime => ({
+              ...anime,
+              thumbnail:
+                this.fileServingService.convertPictureToFileServingPath(
+                  anime.thumbnail
+                ) || defaultThumbnail,
+            }));
+          })
+        )
         .subscribe(animes => {
-          if (animes.error) return;
-          this.animes = animes.data.animesFromFolder;
+          this.animes = animes;
         });
     }
     console.log("there's duplicate inputs");
