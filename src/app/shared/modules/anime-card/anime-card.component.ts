ab55dda956becc7d5e9ee07c7e9837299333d83b
Peter A. Labib <peteramir5522@outlook.com>
2024-09-10 16:22:01 +0300

    fix: Anime card routing in anime viewer

--- src/app/shared/modules/anime-card/anime-card.component.ts
+++ src/app/shared/modules/anime-card/anime-card.component.ts
@@ -1,12 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Anime, AnimeFf } from 'src/app/core/services/graphql.service';
 
 @Component({
   selector: 'app-anime-card',
   templateUrl: './anime-card.component.html',
   styleUrls: ['./anime-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AnimeCardComponent {
   @Input() anime: Omit<Anime, 'createdAt' | 'updatedAt'> | AnimeFf;
-  constructor() {}
+  @Input({ required: true }) navigatable: boolean;
+
+  private readonly router: Router = inject(Router);
+
+  navigateToAnimePage(id: string): void {
+    if (this.navigatable === true) {
+      this.router.navigate(['/library', 'details', id]);
+    }
+  }
 }
