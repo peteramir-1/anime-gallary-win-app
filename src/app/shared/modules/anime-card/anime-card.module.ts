80530be6ebe4db90c9eec838e4e9e490bee9bb9a
Peter A. Labib <peteramir5522@outlook.com>
2024-11-05 12:03:06 +0200

    feat: add mat tooltip for large anime names

--- src/app/shared/modules/anime-card/anime-card.module.ts
+++ src/app/shared/modules/anime-card/anime-card.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { AnimeCardComponent } from './anime-card.component';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [AnimeCardComponent],
-  imports: [CommonModule, RouterModule, NgOptimizedImage],
+  imports: [CommonModule, RouterModule, NgOptimizedImage, MatTooltipModule],
   exports: [AnimeCardComponent],
 })
 export class AnimeCardModule {}
