926bdc8b0e42c5fbd2a21371a31d7d893a62434c
Peter A. Labib <peteramir5522@outlook.com>
2024-11-10 14:54:42 +0200

    cleanup: remove previous used servers

--- anime-gallery-electron/src/servers/frontend/routes/serving-files.ts
+++
@@ -1,8 +0,0 @@
-import * as express from 'express';
-import { servePictures } from '../handlers/serve-pictures';
-
-const router = express.Router();
-
-router.get('/picture', servePictures);
-
-export default router;
