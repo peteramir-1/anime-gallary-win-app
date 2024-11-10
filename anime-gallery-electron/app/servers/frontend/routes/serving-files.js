c2338c4b8ea01091b29ac303fbefb8deded20498
Peter A. Labib <peteramir5522@outlook.com>
2024-11-10 11:00:34 +0200

    update: remove previously compiled appplication

--- anime-gallery-electron/app/servers/frontend/routes/serving-files.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const serve_pictures_1 = require("../handlers/serve-pictures");
-const router = express.Router();
-router.get('/picture', serve_pictures_1.servePictures);
-exports.default = router;
-//# sourceMappingURL=serving-files.js.map
