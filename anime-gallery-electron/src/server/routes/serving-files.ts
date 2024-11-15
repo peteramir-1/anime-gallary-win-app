d3217d09584b4609f41e5e00817ccae5d7e3a6b9
Peter A. Labib <peteramir5522@outlook.com>
2024-11-15 23:51:18 +0200

    feat(api): Add serve videos route
    Create na new route in api for video streaming

--- anime-gallery-electron/src/server/routes/serving-files.ts
+++ anime-gallery-electron/src/server/routes/serving-files.ts
@@ -1,8 +1,11 @@
 import express, { Router } from 'express';
 import { servePictures } from '../controllers/serve-pictures';
+import { serveVideo } from '../controllers/serve-videos';
 
 const router: Router = express.Router();
 
 router.get('/picture', servePictures);
+
+router.get('/video', serveVideo);
 
 export default router;
