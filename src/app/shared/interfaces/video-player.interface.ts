7dd4028758ee746f66a0d7f47a56f1f1aef6c680
Peter A. Labib <peteramir5522@outlook.com>
2024-11-22 02:14:52 +0200

    refractor(shared interfaces): remove unused shared interfaces (video player settings interface)

--- src/app/shared/interfaces/video-player.interface.ts
+++ src/app/shared/interfaces/video-player.interface.ts
@@ -2,20 +2,3 @@ export interface theme {
   name: string;
   value: string;
 }
-
-export interface VideoPlayerSettings {
-  theme: string;
-  autoplay: boolean;
-  loop: boolean;
-  muted: boolean;
-  pip: boolean;
-  controls: boolean;
-  hotkeys: boolean;
-  enableNumbers: boolean;
-  enableVolumeScroll: boolean;
-  enableModifiersForNumbers: boolean;
-  enableMute: boolean;
-  enableFullscreen: boolean;
-  seekStep: number;
-  volumeStep: number;
-}
