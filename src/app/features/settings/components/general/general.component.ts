f9bb5038cf7934577985fa375c33cc99783c4da6
Peter A. Labib <peteramir5522@outlook.com>
2024-11-23 20:46:36 +0200

    docs(General Component): Generate JsDocs for functions

--- src/app/features/settings/components/general/general.component.ts
+++ src/app/features/settings/components/general/general.component.ts
@@ -11,6 +11,10 @@ export class GeneralComponent {
 
   constructor(private darkModeService: DarkModeService) {}
 
+  /**
+   * Toggles the application's theme between dark and light modes
+   * using the DarkModeService.
+   */
   toggleTheme(): void {
     this.darkModeService.toggleTheme()
   }
