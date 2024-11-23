6ab0e8f66e4fc5aaf0ad5d2a45427b690762a695
Peter A. Labib <peteramir5522@outlook.com>
2024-11-23 20:55:30 +0200

    refractor(video player settings resolver): remove unused parameters in the resolve method

--- src/app/features/settings/resolvers/video-player-settings.resolver.ts
+++ src/app/features/settings/resolvers/video-player-settings.resolver.ts
@@ -1,5 +1,4 @@
 import { inject, Injectable } from '@angular/core';
-import { RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -12,8 +11,6 @@ import { GetVideoPlayerSettingsGQL, GetVideoPlayerSettingsQuery } from 'src/app/
 export class VideoPlayerSettingsResolver  {
   private getVideoPlayerSettingGQL = inject(GetVideoPlayerSettingsGQL);
   resolve(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
   ): Observable<GetVideoPlayerSettingsQuery['settings']> {
     return this.getVideoPlayerSettingGQL
       .fetch()
