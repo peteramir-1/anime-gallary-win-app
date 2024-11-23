8fbe97adfe8d4ead39d463b40e93f0d159f6e553
Peter A. Labib <peteramir5522@outlook.com>
2024-11-23 21:07:21 +0200

    style:(video player settings resolver)

--- src/app/features/settings/resolvers/video-player-settings.resolver.ts
+++ src/app/features/settings/resolvers/video-player-settings.resolver.ts
@@ -3,17 +3,20 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { GetVideoPlayerSettingsGQL, GetVideoPlayerSettingsQuery } from 'src/app/core/services/graphql.service';
+import {
+  GetVideoPlayerSettingsGQL,
+  GetVideoPlayerSettingsQuery,
+} from 'src/app/core/services/graphql.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class VideoPlayerSettingsResolver  {
+export class VideoPlayerSettingsResolver {
   private getVideoPlayerSettingGQL = inject(GetVideoPlayerSettingsGQL);
-  resolve(
-  ): Observable<GetVideoPlayerSettingsQuery['settings']> {
+
+  resolve(): Observable<GetVideoPlayerSettingsQuery['settings']> {
     return this.getVideoPlayerSettingGQL
       .fetch()
-      .pipe(map(res => (res.data.settings)));
+      .pipe(map(res => res.data.settings));
   }
 }
