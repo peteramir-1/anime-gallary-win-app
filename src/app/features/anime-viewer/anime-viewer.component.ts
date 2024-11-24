9a3af33c9cd638a6605c2eef863d8c29f4933308
Peter A. Labib <peteramir5522@outlook.com>
2024-11-24 16:05:17 +0200

    refractor(anime viewer component): change form value subscribtion and instead use the rxjs pipes and subscriptions only
    - remove form builder injection
    - remove animeFolder form control and convert it into normal property with type string
    - remove anime folder subscription to update anime list and instead use the button and electron subscription with swichMap to send the request with anime data for fast performance and clearer feature implementation

--- src/app/features/anime-viewer/anime-viewer.component.ts
+++ src/app/features/anime-viewer/anime-viewer.component.ts
@@ -1,15 +1,11 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  DestroyRef,
   inject,
-  OnInit,
   signal,
 } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { FormBuilder } from '@angular/forms';
 
-import { map } from 'rxjs/operators';
+import { filter, map, switchMap } from 'rxjs/operators';
 
 import { ElectronService } from 'src/app/core/services/electron.service';
 import { GetAnimesFromFolderGQL } from 'src/app/core/services/graphql.service';
@@ -22,53 +18,43 @@ import { HelperService } from 'src/app/shared/services/helper.service';
   styleUrl: './anime-viewer.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AnimeViewerComponent implements OnInit {
+export class AnimeViewerComponent {
   private readonly electronService = inject(ElectronService);
   private readonly getAnimesFromFolderGQL = inject(GetAnimesFromFolderGQL);
-  private readonly formBuilder = inject(FormBuilder);
-  private readonly destroyRef = inject(DestroyRef);
 
-  readonly animesFolder = this.formBuilder.control<string | undefined>(
-    undefined
-  );
-
-  private readonly prevAnimeFolder = signal(undefined);
-
+  readonly animesFolderPath = signal<string>('');
   readonly animes = signal<AnimeFf[]>([]);
   readonly error = signal<string | undefined>(undefined);
 
   searchText = '';
 
-  ngOnInit(): void {
-    this.animesFolder.valueChanges
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(() => this.updateAnimesList());
-  }
-
-  private updateAnimesList(): void {
-    if (this.animesFolder.value !== this.prevAnimeFolder()) {
-      this.getAnimesFromFolderGQL
-        .fetch({ folderPath: this.animesFolder.value })
-        .pipe(map(res => res?.data?.animesFromFolder || []))
-        .subscribe(
-          (animes: AnimeFf[]) => {
-            this.error.set(undefined);
-            this.animes.set(animes);
-          },
-          err => {
-            this.error.set(err.message);
-            this.animes.set([]);
-          }
-        );
-    }
-    this.prevAnimeFolder.set(this.animesFolder.value);
-  }
-
-  selectAnimeFolder(): void {
-    this.electronService.selectFolder().subscribe((path: string | false) => {
-      if (!path || path === '') return;
-      this.animesFolder.setValue(path);
-    });
+  getAnimeData(): void {
+    this.electronService
+      .selectFolder()
+      .pipe(
+        filter(folderPath => !!folderPath && folderPath !== ''),
+        switchMap(folderPath =>
+          this.getAnimesFromFolderGQL
+            .fetch({ folderPath })
+            .pipe(
+              map(res => ({
+                folderPath,
+                animes: res?.data?.animesFromFolder || [],
+              }))
+            )
+        )
+      )
+      .subscribe(
+        ({ folderPath, animes }) => {
+          this.error.set(undefined);
+          this.animes.set(animes);
+          this.animesFolderPath.set(folderPath);
+        },
+        err => {
+          this.error.set(err.message);
+          this.animes.set([]);
+        }
+      );
   }
 
   readonly divideArrayIntoSubarrays =
