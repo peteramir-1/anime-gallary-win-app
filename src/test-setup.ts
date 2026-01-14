f0df99aa655dc81cc4690cf8ae56d9ad512de1a5
Peter A. Labib <134029341+peteramir-1@users.noreply.github.com>
2026-01-14 19:23:40 +0200

    chore: replace Karma with Vitest

---
+++ src/test-setup.ts
@@ -0,0 +1,14 @@
+// This file is required by vitest.config.js and loads recursively all the .spec and framework files
+
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserTestingModule,
+  platformBrowserTesting,
+} from '@angular/platform-browser/testing';
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserTestingModule,
+  platformBrowserTesting()
+);
